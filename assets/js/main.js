901cbdb7b24b1fcc86264aed3521daa0123cc0a1
Daniel <DanielOrchard@users.noreply.github.com>
2024-02-16 15:23:08 +0000

    draft with most working, updates needed still on tut marketplace

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Initialize filter buttons
     var filterButtons = document.querySelectorAll('.filter-button');
     filterButtons.forEach(function(button) {
         button.addEventListener('click', function() {
             filterButtons.forEach(btn => btn.classList.remove('active')); // Remove active class from all buttons
             this.classList.add('active'); // Add active class to the clicked button
+            // Retrieve filter value from data-filter attribute
+            var filterValue = this.getAttribute('data-filter');
+            // Apply filter
+            mixer.filter(filterValue);
         });
     });
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var container = document.querySelector('.portfolio-container');
-        
-        if(container) { // Only initialize MixItUp if .portfolio-container exists
-            var mixer = mixitup(container);
-        }
-    });
+    // Initialize MixItUp for the .portfolio-container if it exists
+    var container = document.querySelector('.portfolio-container');
+    if (container) {
+        var mixer = mixitup(container);
+    }
 
     // Initialize AOS
     AOS.init({
-        // AOS options here
         easing: 'ease-in-out',
         duration: 1000,
-        once: true // Whether animation should happen only once - while scrolling down
+        once: true
     });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Trigger width adjustments after AOS animations complete
+    // Progress bar width adjustments after AOS animations complete
     document.querySelectorAll('.progress-bar').forEach(function(bar) {
-        // Use setTimeout to allow AOS animation to complete before filling the bar
         setTimeout(function() {
             const value = bar.getAttribute('aria-valuenow');
             bar.style.width = value + '%';
-        }, 800); // Adjust the timeout to match or exceed AOS animation duration
+        }, 800); // Adjust the timeout to sync with AOS animation
     });
 });
